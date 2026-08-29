ab8028cf0ed5dc23a89bf2a8e4bfb158899bcc8e
travsif <travsif@users.noreply.github.com>
2026-08-29 11:29:20 -0700

    chore: standardize android package and application id to com.hometrust.app and build release bundles

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.homeverify.app"
+    namespace = "com.hometrust.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.homeverify.app"
+        // Application ID registered on Google Play Console
+        applicationId = "com.hometrust.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- mobile_app/android/app/src/main/kotlin/com/hometrust/app/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/hometrust/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.homeverify.app
+package com.hometrust.app
 
 import io.flutter.embedding.android.FlutterActivity
 
